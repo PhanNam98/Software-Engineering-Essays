4542f9cbabde40646947bd41fe0623b8ed419e47
PhanNam98 <43126684+PhanNam98@users.noreply.github.com>
2019-10-09 13:58:47 +0700

    Init Identity .Netcore

---
+++ Source Code/Source code/.Net Core/BDS_ML/BDS_ML/Models/ApplicationRole.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDS_ML.Models
+{
+    public class ApplicationRole:IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string roleName) : base(roleName) {
+        }
+        public ApplicationRole(string roleName,string description,DateTime creationDate) : base(roleName) {
+            this.CreationDate = creationDate;
+            this.Description = description;
+        }
+        public string Description;
+        public DateTime CreationDate;
+    }
+}
---
+++ Source Code/Source code/.Net Core/BDS_ML/BDS_ML/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDS_ML.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public ApplicationUser() : base() { }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int IsBlock { get; set; }
+        public int IsAdmin { get; set; }
+
+    }
+}
