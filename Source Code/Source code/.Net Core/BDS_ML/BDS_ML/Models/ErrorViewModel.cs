d5d579b8c5f165bfe0b9f2e8fa82a0e575d89a9f
PhanNam98 <43126684+PhanNam98@users.noreply.github.com>
2019-10-09 11:17:21 +0700

    Init .Net Core
    Create Project BDS_ML

---
+++ Source Code/Source code/.Net Core/BDS_ML/BDS_ML/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BDS_ML.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
