2680e8f197508db04500f37f83d9edd984467ed9
thienquoc98 <thienquoc98@gmail.com>
2019-12-03 16:51:50 +0700

    update css, layout

--- Source Code/Source code/.Net Core/BDS_ML/BDS_ML/Controllers/HomeController.cs
+++ Source Code/Source code/.Net Core/BDS_ML/BDS_ML/Controllers/HomeController.cs
@@ -22,6 +22,22 @@ namespace BDS_ML.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Join()
+        {
+            return View();
+        }
+        public IActionResult Advertisement()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
